379f77aa85a57a6b0650d06f6bc1814bc031517c
Eric Fuller <info@glassmoonmedia.com>
2016-12-27 11:02:06 -0500

    Add some comments and a few other code enhancements.

--- signup/js/signup.js
+++ signup/js/signup.js
@@ -67,86 +67,131 @@ window.EPF_SignupForm = {};
 		helpers.hideMessage(this, app.config.validationClass);
 	};
 
+	/**
+	 * Validate the first name input.
+	 *
+	 * @returns {boolean}
+	 */
 	app.validateFirstName = function() {
 		var firstName = app.c.firstName.value;
 
 		// If empty.
 		if (validator.isEmpty(firstName)) {
 			helpers.showMessage(app.c.firstName, app.config.validationClass, 'Please enter a first name.');
-			return;
+			return false;
 		}
 
 		// If not at least 3 characters long.
 		if (!validator.isOfLength(firstName, app.config.minCharacters)) {
 			helpers.showMessage(app.c.firstName, app.config.validationClass, 'First name needs to be at least 3 characters.');
+			return false;
 		}
+
+		return true;
 	};
 
+	/**
+	 * Validate the last name input.
+	 *
+	 * @returns {boolean}
+	 */
 	app.validateLastName = function() {
 		var lastName = app.c.lastName.value;
 
 		// If empty.
 		if (validator.isEmpty(lastName)) {
 			helpers.showMessage(app.c.lastName, app.config.validationClass, 'Please enter a last name.');
-			return;
+			return false;
 		}
 
 		// If not at least 3 characters long.
 		if (!validator.isOfLength(lastName, app.config.minCharacters)) {
 			helpers.showMessage(app.c.lastName, app.config.validationClass, 'Last name needs to be at least 3 characters.');
+			return false;
 		}
+
+		return true;
 	};
 
+	/**
+	 * Validate the email input.
+	 *
+	 * @returns {boolean}
+	 */
 	app.validateEmail = function() {
 		var email = app.c.email.value;
 
 		// If empty.
 		if (validator.isEmpty(email)) {
 			helpers.showMessage(app.c.email, app.config.validationClass, 'Please enter an email address.');
-			return;
+			return false;
 		}
 
 		// If a valid email address
 		if (!validator.isEmailAddress(email)) {
 			helpers.showMessage(app.c.email, app.config.validationClass, 'Please enter a valid email address.');
+			return false;
 		}
+
+		return true;
 	};
 
+	/**
+	 * Validate the outfitter input.
+	 *
+	 * @returns {boolean}
+	 */
 	app.validateOutfitterName = function() {
 		var outfitterName = app.c.outfitter.value;
 
 		if (validator.isEmpty(outfitterName)) {
 			helpers.showMessage(app.c.outfitter, app.config.validationClass, 'Please enter an outfitter name.');
-			return;
+			return false;
 		}
 
 		// If not at least 3 characters long.
 		if (!validator.isOfLength(outfitterName, app.config.minCharacters)) {
 			helpers.showMessage(app.c.outfitter, app.config.validationClass, 'Name needs to be at least 3 characters.');
+			return false;
 		}
+
+		return true;
 	};
 
+	/**
+	 * Validate the date of birth input.
+	 *
+	 * @returns {boolean}
+	 */
 	app.validateDOB = function() {
 		var dob = app.c.dob.value;
 
 		// If empty.
 		if (validator.isEmpty(dob)) {
 			helpers.showMessage(app.c.dob, app.config.validationClass, 'Please enter a date of birth.');
-			return;
+			return false;
 		}
 
 		// If not a valid email address.
 		if (!validator.isDate(dob)) {
 			helpers.showMessage(app.c.dob, app.config.validationClass, 'Please enter a valid date of birth.');
-			return;
+			return false;
 		}
 
 		// Must be at least 13 years of age.
 		if (!helpers.isOfAge(dob, 13)) {
 			helpers.showMessage(app.c.dob, app.config.validationClass, 'You must be at least 13 to register.');
+			return false;
 		}
+
+		return true;
 	};
 
+	/**
+	 * Validate the passwords and make sure they match.
+	 *
+	 * @returns {boolean}
+	 */
 	app.validatePasswords = function() {
 		var password = app.c.password.value;
 		var confirmPassword = app.c.confirmPassword.value;
@@ -154,27 +199,30 @@ window.EPF_SignupForm = {};
 		// If empty.
 		if(validator.isEmpty(password)) {
 			helpers.showMessage(app.c.password, app.config.validationClass, 'Please enter a password.');
-			return;
+			return false;
 		}
 
 		// If empty.
 		if (validator.isEmpty(confirmPassword)) {
 			helpers.showMessage(app.c.confirmPassword, app.config.validationClass, 'Please confirm your password.');
-			return;
+			return false;
 		}
 
 		// Passwords must be at least 6 characters long.
 		if (!validator.isOfLength(password, 6) || !validator.isOfLength(confirmPassword, 6)) {
 			helpers.showMessage(app.c.password, app.config.validationClass, 'Passwords must be at least 6 characters long.');
 			helpers.showMessage(app.c.confirmPassword, app.config.validationClass, 'Passwords must be at least 6 characters long.');
-			return;
+			return false;
 		}
 
 		// Compare passwords.
 		if (!helpers.comparePasswords(password, confirmPassword)) {
 			helpers.showMessage(app.c.password, app.config.validationClass, 'Passwords to not match.');
 			helpers.showMessage(app.c.confirmPassword, app.config.validationClass, 'Passwords to not match.');
+			return false;
 		}
+
+		return true;
 	};
 
 	/**
@@ -185,18 +233,33 @@ window.EPF_SignupForm = {};
 	 * @returns {boolean}
 	 */
 	app.validate = function(e) {
+		var valid = false;
 
 		// Prevent default submit action.
 		e.preventDefault();
 
 		// Validate fields.
-		app.validateFirstName();
-		app.validateLastName();
-		app.validateEmail();
-		app.validateOutfitterName();
-		app.validateDOB();
-		app.validatePasswords();
+		var firstName = app.validateFirstName();
+		var lastName = app.validateLastName();
+		var email = app.validateEmail();
+		var outfitterName = app.validateOutfitterName();
+		var dob = app.validateDOB();
+		var passwords = app.validatePasswords();
+
+		// If everything validates.
+		if (firstName && lastName && email && outfitterName && dob && passwords) {
+			valid = true;
+		}
+
+		// Bail if valid still equals false.
+		if (valid === false) {
+			return false;
+		}
+
+		// Go ahead and submit the form!
+		console.log('Submit the form!');
 	};
 
+	// Engage!
 	app.init();
 })(window, document, window.EPF_SignupForm, validator, helpers);
